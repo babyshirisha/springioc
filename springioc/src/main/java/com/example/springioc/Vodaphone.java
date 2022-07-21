9d4f0c9f907fd23913587c820246acc5ca8d0c0c
babyshirisha <babyshirisha24@gmail.com>
2022-07-21 23:27:58 +0530

    example of springioc

---
+++ springioc/src/main/java/com/example/springioc/Vodaphone.java
@@ -0,0 +1,15 @@
+package com.example.springioc;
+
+public class Vodaphone implements Sim{
+
+	@Override
+	public void calling() {
+		System.out.println("calling using vodaphone sim");
+	}
+
+	@Override
+	public void data() {
+		System.out.println("browsing internet using vodaphone sim");
+		}
+
+}
